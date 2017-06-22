514a7da9ea708080b493f34362b5c0b25ab98f79
Voidstorm <sasha.yaroshepta@gmail.com>
2017-06-22 12:41:44 +0300

    Heh

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello hui!')
 })
 
 app.listen(8080, function () {
